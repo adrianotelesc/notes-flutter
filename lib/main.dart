1d64ad33582dc9334ad9f44c1f557d18a560abb6
Adriano Teles <adriano.telesc@gmail.com>
2024-05-26 09:04:17 -0400

    Refactor plugins

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:postnote/app.dart';
 import 'package:postnote/dependencies.dart';
-import 'package:postnote/plugins.dart'
-    if (dart.library.html) 'package:postnote/web_plugins.dart'
-    if (dart.library.io) 'package:postnote/plugins.dart';
+import 'package:postnote/plugins/plugins.dart'
+    if (dart.library.html) 'package:postnote/plugins/web_plugins.dart'
+    if (dart.library.io) 'package:postnote/plugins/plugins.dart';
 
 void main() {
   setUpPlugins();
