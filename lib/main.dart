55125b88545f356de01761c13cba7befe9052e8e
Adriano Teles <adriano.telesc@gmail.com>
2024-05-30 15:48:23 -0400

    Move app, routing and theming to ui folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:postnote/app.dart';
+import 'package:postnote/ui/app.dart';
 import 'package:postnote/dependencies.dart';
 import 'package:postnote/plugins/plugins.dart'
     if (dart.library.html) 'package:postnote/plugins/web_plugins.dart'
--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import 'package:postnote/theming.dart';
-import 'package:postnote/routing.dart';
+import 'package:postnote/ui/theming.dart';
+import 'package:postnote/ui/routing.dart';
 
 class PostnoteApp extends StatelessWidget {
   PostnoteApp({super.key});
