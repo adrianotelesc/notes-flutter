b1896a0bcd864837a427228231ca5e9e95c7a031
Adriano Teles <adriano.telesc@gmail.com>
2024-07-23 14:27:19 -0300

    Prevent rebuild to create new router

--- lib/ui/routing.dart
+++ lib/ui/routing.dart
@@ -15,90 +15,88 @@ abstract class PostnoteRouting {
 
   static const _transitionDuration = Duration(milliseconds: 200);
 
-  static RouterConfig<Object> get routerConfig {
-    return GoRouter(
-      routes: [
-        GoRoute(
-          path: '/',
-          pageBuilder: (_, state) {
-            return MaterialPage<void>(
-              key: state.pageKey,
-              child: const HomePage(),
-            );
-          },
-          routes: [
-            GoRoute(
-              path: ':boardId',
-              pageBuilder: (_, state) {
-                final boardId = state.pathParameters['boardId'] ?? '';
+  static RouterConfig<Object> routerConfig = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        pageBuilder: (_, state) {
+          return MaterialPage<void>(
+            key: state.pageKey,
+            child: const HomePage(),
+          );
+        },
+        routes: [
+          GoRoute(
+            path: ':boardId',
+            pageBuilder: (_, state) {
+              final boardId = state.pathParameters['boardId'] ?? '';
 
-                return MaterialPage<void>(
-                  key: state.pageKey,
-                  child: NoteListPage(boardId: boardId),
-                );
-              },
-              routes: [
-                GoRoute(
-                  path: ':noteId',
-                  pageBuilder: (context, state) {
-                    final isSmallScreen = ScreenUtils.isSmallScreen(context);
+              return MaterialPage<void>(
+                key: state.pageKey,
+                child: NoteListPage(boardId: boardId),
+              );
+            },
+            routes: [
+              GoRoute(
+                path: ':noteId',
+                pageBuilder: (context, state) {
+                  final isSmallScreen = ScreenUtils.isSmallScreen(context);
 
-                    final boardId = state.pathParameters['boardId'] ?? '';
-                    final noteId = state.pathParameters['noteId'];
+                  final boardId = state.pathParameters['boardId'] ?? '';
+                  final noteId = state.pathParameters['noteId'];
 
-                    return CustomTransitionPage<void>(
-                      key: state.pageKey,
-                      fullscreenDialog: isSmallScreen,
-                      child: isSmallScreen
-                          ? Dialog.fullscreen(
-                              insetAnimationDuration: _transitionDuration,
+                  return CustomTransitionPage<void>(
+                    key: state.pageKey,
+                    fullscreenDialog: isSmallScreen,
+                    child: isSmallScreen
+                        ? Dialog.fullscreen(
+                            insetAnimationDuration: _transitionDuration,
+                            child: NoteDetailPage(
+                              boardId: boardId,
+                              noteId: noteId,
+                            ),
+                          )
+                        : Dialog(
+                            clipBehavior: Clip.antiAlias,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(_dialogCornerRadius),
+                              ),
+                            ),
+                            insetPadding: const EdgeInsets.symmetric(
+                              horizontal: _dialogHorizontalInsetPadding,
+                              vertical: kToolbarHeight,
+                            ),
+                            child: ConstrainedBox(
+                              constraints: const BoxConstraints(
+                                maxWidth: _dialogMaxSize,
+                                maxHeight: _dialogMaxSize,
+                                minHeight: _dialogMinHeight,
+                              ),
                               child: NoteDetailPage(
                                 boardId: boardId,
                                 noteId: noteId,
                               ),
-                            )
-                          : Dialog(
-                              clipBehavior: Clip.antiAlias,
-                              shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(_dialogCornerRadius),
-                                ),
-                              ),
-                              insetPadding: const EdgeInsets.symmetric(
-                                horizontal: _dialogHorizontalInsetPadding,
-                                vertical: kToolbarHeight,
-                              ),
-                              child: ConstrainedBox(
-                                constraints: const BoxConstraints(
-                                  maxWidth: _dialogMaxSize,
-                                  maxHeight: _dialogMaxSize,
-                                  minHeight: _dialogMinHeight,
-                                ),
-                                child: NoteDetailPage(
-                                  boardId: boardId,
-                                  noteId: noteId,
-                                ),
-                              ),
                             ),
-                      transitionDuration: _transitionDuration,
-                      reverseTransitionDuration: _transitionDuration,
-                      barrierColor: Colors.black54,
-                      barrierDismissible: true,
-                      opaque: false,
-                      transitionsBuilder: (context, animation, _, child) {
-                        return FadeScaleTransition(
-                          animation: animation,
-                          child: child,
-                        );
-                      },
-                    );
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
-      ],
-    );
-  }
+                          ),
+                    transitionDuration: _transitionDuration,
+                    reverseTransitionDuration: _transitionDuration,
+                    barrierColor: Colors.black54,
+                    barrierDismissible: true,
+                    opaque: false,
+                    transitionsBuilder: (context, animation, _, child) {
+                      return FadeScaleTransition(
+                        animation: animation,
+                        child: child,
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  );
 }
